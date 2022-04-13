07fc501287d0d89413e8aee937d521c668842292
Juan Manuel Russo <64755566+Rossu997@users.noreply.github.com>
2022-04-13 17:16:31 -0300

    Actualizacion

--- desafio2_al.js
+++ desafio2_al.js
@@ -1,43 +1,29 @@
 /**
- * DESAFÍO PRINCIPAL 2 
+ * DESAFÍO PRINCIPAL 2
  * Juan Manuel Russo
  * Curso JS CODER - 06/04/2022
-*/
+ */
 
 //! Articles List
 
+//  (Clase Constructora) Lista de todos los articulos cargados en el sitio.
 
 class Articulo {
-  constructor( id, imagenURL, nombre, descripcion, precio ){
+  constructor(id, imagenURL, nombre, descripcion, precio) {
     this.id = id;
     this.imagenURL = imagenURL;
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.precio = precio;
     this.cantidad = 1;
-    this.cantidadTotal = 1;
   }
-
-  cantidadTotal() {
-    return this.cantidadTotal * this.precio;
-  };
 }
 
+//  Creando ista de todos los articulos cargados en el sitio.
 
-
-
-/**----------------------------------------------------------------------*/
-
-
-const dataBaseJSON = JSON.parse( localStorage.getItem( 'articulos' ));
+const localStorageDataBase = JSON.parse(localStorage.getItem("articulos"));
 let listaArticulos = [];
 
-dataBaseJSON.forEach( articulo => {
-    listaArticulos.push( articulo );
+localStorageDataBase.forEach((articulo) => {
+  listaArticulos.push(articulo);
 });
-
-for (let i = 0; i < dataBaseJSON.length; i++) {
-        listaArticulos[i].cantidad = 1;
-        listaArticulos[i].cantidadTotal = 1;
-};
-
