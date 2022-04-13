32c9960bbb46de9b8cde165db054aa3ce8ce3455
Juan Manuel Russo <64755566+Rossu997@users.noreply.github.com>
2022-04-13 15:43:15 -0300

    Update desafio2.js
    Implementacion de nuevas funciones:
    Sumar y restar unidades en el carrito.
    Precio total de articulo (considerando unidades).
    Precio final de la compra.

--- desafio2.js
+++ desafio2.js
@@ -1,86 +1,149 @@
 /**
- * DESAFÍO PRINCIPAL 2 
+ * DESAFÍO PRINCIPAL 2
  * Juan Manuel Russo
  * Curso JS CODER - 06/04/2022
-*/
+ */
 
 //! Main Script
 
 
-//  Objeto constructor y métodos para la lista de productos
-const carritoLista = document.getElementById( 'carrito-lista' );
-const buttons = document.getElementsByClassName( 'btn-add' );
+//?  (Clase Constructora) Lista de IDs
+const HTMLArticulosEnCarrito = document.getElementById("carrito-lista");
+
 class Carrito {
-  constructor(){
-    this.articulos = [];
-  };
+  constructor() {
+    this.idsArticulosEnCarrito = [];
+    this.cantidadesDeCadaArticulo = {};
+    this.precioFinalDeCompra = 0;
+  }
 
-  pushArticulo( idArticulo ){
-    this.articulos.push( idArticulo )
-  };
+  //Agrega un nuevo articulo (ID) al carrito ('idsArticulosEnCarrito'(ARRAY de IDs))
+  sumarUnidadArticulo(idArticulo) {
+    idArticulo = parseInt(idArticulo);
 
-  getArticulos(){
-    return this.articulos;
-  };
+    //
+    if (this.idsArticulosEnCarrito.indexOf(idArticulo) !== -1) {
+      this.cantidadesDeCadaArticulo[idArticulo] = this.cantidadesDeCadaArticulo[idArticulo] + 1;
+    } else {
+      this.idsArticulosEnCarrito.push(idArticulo);
+      this.cantidadesDeCadaArticulo[idArticulo] = 1;
+    }
+    this.renderCarrito();
+  }
 
-  renderCarrito(){
-    const data = this.getDataArticulo();
+  //Restar una unidad a un articulo del carrito.
+  restarUnidadArticulo(idArticulo) {
+    idArticulo = parseInt(idArticulo);
 
-    carritoLista.innerHTML = "";
+    //
+    if( this.idsArticulosEnCarrito.indexOf( idArticulo !== -1 )) {
+      this.cantidadesDeCadaArticulo[idArticulo] = this.cantidadesDeCadaArticulo[idArticulo] - 1;
+    }
+    else {
+      return;
+    }
+    this.renderCarrito();
+  }
 
-    data.forEach((producto) => { 
-    carritoLista.innerHTML += `
-        <li class="carrito-item"><span class="borrar-carrito-item">x</span> ${producto.nombre} $${producto.precio}
-        <p class="carrito-item-cantidad">Cantidad total: ${producto.cantidadTotal}</p></li>`;
-    })
-  };
+  //Quitar todas las unidades de un articulo del carrito.
+  quitarArticulo( idArticulo ) {
+    
+    idArticulo = parseInt(idArticulo);
+    
+    this.idsArticulosEnCarrito = this.idsArticulosEnCarrito.filter((id) => id !== idArticulo);
 
-  getDataArticulo(){
-    return this.articulos.map(( idArticulo ) => { 
-      const articulo = listaArticulos.find(( articulo ) => articulo.id === parseInt( idArticulo ));
+    delete this.cantidadesDeCadaArticulo[idArticulo]
+
+    this.renderCarrito()
+
+  }
+
+  //Devuelve los articulos (IDs) que estan guardados en el carrito ('idsArticulosEnCarrito').
+  consultarArticulosEnCarrito() {
+    return this.idsArticulosEnCarrito;
+  }
+
+  calcularPrecioTotalArticulo(articulo){
+    return articulo.precio * this.cantidadesDeCadaArticulo[articulo.id];
+
+  }
+
+  //Crea en el HTML las etiquetas que muestran los articulos que se agregan al carrito.
+  renderCarrito() {
+   
+    this.calcularPrecioFinalDeCompra()
+    const datos = this.consultarDatosArticulo();
+    HTMLArticulosEnCarrito.innerHTML = "";
+    datos.forEach((articulo) => {
+      HTMLArticulosEnCarrito.innerHTML += `
+        <li class="carrito-item"><span class="borrar-carrito-item"  onClick="carrito.quitarArticulo(${articulo.id})">x</span> ${articulo.nombre} $${carrito.calcularPrecioTotalArticulo(articulo)}<p class="carrito-item-cantidad">Cantidad total: <span onClick="carrito.sumarUnidadArticulo(${articulo.id})"  class="sumar-unidad-item-carrito">+</span> ${carrito.cantidadesDeCadaArticulo[articulo.id]} 
+        <span class="restar-unidad-item-carrito" onClick="carrito.restarUnidadArticulo(${articulo.id})">-</span></p></li>
+
+
+        ${carrito.precioFinalDeCompra}
+        `;
+    });
+  }
+
+  calcularPrecioFinalDeCompra() {
+    const datos  =  this.consultarDatosArticulo();
+
+    this.precioFinalDeCompra = 0
+
+    datos.forEach((articulo) => {
+      this.precioFinalDeCompra = this.precioFinalDeCompra + articulo.precio * this.cantidadesDeCadaArticulo[articulo.id];
+     })
+  }
+
+  aplicarCuponDescuento(){
+    this.precioFinalDeCompra = this.precioFinalDeCompra * 0.2
+  }
+
+  //Busca y devuelve los datos de los articulos usando los IDs que estan en el carrito ('idsArticulosEnCarrito').
+  consultarDatosArticulo() {
+    return this.idsArticulosEnCarrito.map((idArticulo) => {
+      const articulo = listaArticulos.find(
+        (articulo) => articulo.id === parseInt(idArticulo)
+      );
       return articulo;
-    })
-  };
+    });
+  }
 
-  clearCarrito(){
-    carritoLista.innerHTML = "";
-    this.articulos = [];
-  };
-};
+  //Borra todo el contenido del carrito ('idsArticulosEnCarrito', 'cantidadesDeCadaArticulo' y HTML)
+  limpiarCarrito() {
 
-const carrito =  new Carrito();
+    if( confirm( "Vas a borrar todos tus artículos del carrito. ¿Estás seguro?" )){
+      HTMLArticulosEnCarrito.innerHTML = "";
+      this.idsArticulosEnCarrito = [];
+      this.cantidadesDeCadaArticulo = {};
+    };
+    
+  }
+}
 
 
-//  Render listado producto
-const containerTarjetas = document.getElementById( 'container-tarjetas' );
+//? Creando el carrito
+const carrito = new Carrito();
 
-for( let i = 0; i < listaArticulos.length; i++ ){
-    containerTarjetas.innerHTML += `
+
+//? Render listado producto
+
+const HTMLContainerTarjetas = document.getElementById("container-tarjetas");
+
+for (let i = 0; i < listaArticulos.length; i++) {
+  HTMLContainerTarjetas.innerHTML += `
       <div class="tarjeta-normal">
         <img src="${listaArticulos[i].imagenURL}" alt="${listaArticulos[i].nombre}">
         <h3 class="precio">${listaArticulos[i].nombre}</h3>
         <p>${listaArticulos[i].descripcion}</p>
         <p class="precio">Precio: $${listaArticulos[i].precio}</p>
-        <button id=${listaArticulos[i].id} class="btn-add" type="button" >¡AGREGAR!</button>
+        <button id=${listaArticulos[i].id} class="btn-add" type="button" onClick="carrito.sumarUnidadArticulo(${listaArticulos[i].id})"  >¡AGREGAR!</button>
       </div>`;
-};
+}
 
+//? Evento en el boton Limpiar todo el carrito (Borrar Todo)
 
-//Eventos en el boton Agregar
- for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click',  function(event){ 
-      carrito.pushArticulo(event.target.id)
-     carrito.renderCarrito()
-     });
-};
-
-
-//Evento en el boton limpiar carrito
-document.getElementById( 'btn-quitar-todo' ).addEventListener( 'click', function(){
-  if( confirm( 'Vas a borrar todos tus artículos del carrito. ¿Estás seguro?' )){
-    carrito.clearCarrito();
-  };
-});
+document.getElementById("btn-quitar-todo").addEventListener("click", carrito.limpiarCarrito);
 
 
 
